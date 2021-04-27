d4ea7c3f2e3429351c2364a52d893206d4c29509
郭庆 <31536879@qq.com>
2021-04-27 18:11:10 +0800

    update

--- test/resolve.js
+++ test/resolve.js
@@ -12,4 +12,15 @@ Promise.race([p1, p2, p3]).then((res) => {
   console.log(res);
 });
  */
-const Promise 
+const Promise = require('./promiseA+')
+
+
+Promise.resolve().then(() => {
+  console.log('ok1')
+  return new Promise(()=>{})  // 返回“pending”状态的Promise对象
+}).then(() => {
+  // 后续的函数不会被调用
+  console.log('ok2')
+}).catch(err => {
+  console.log('err->', err)
+})
